0b8babbf8a2f98e8be7d516d497bd94c3fa308f6
Lê Thanh Nhã <tnhalk@maytinhdian.com>
2023-10-05 10:10:07 +0700

    Tao du lieu mau cho cac Table 95 Finish

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        $groupId = DB::table('groups')->insertGetId([
+            'name' => 'Administrator',
+            'user_id' => 0,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+        if ($groupId > 0) {
+          $userId=  DB::table('users')->insertGetId([
+                'name' => 'Thanh Nhã',
+                'email' => 'tnhalk@maytinhdian.com',
+                'password' => Hash::make('123456'),
+                'group_id' =>$groupId,
+                'user_id'=>0,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]);
+        }
+        if($userId>0){
+            for ($i=0; $i <=5 ; $i++) { 
+                DB::table('posts')->insert([
+                    'title'=>'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s',
+                    'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                    'user_id' =>$userId,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ]);
+            }
+            
+        }
     }
 }
